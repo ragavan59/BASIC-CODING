9ed53f65e84dafb2572e9771007ec348651ab993
ragavan59 <ragavan3518@gmail.com>
2025-09-19 10:05:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-var product=("apple","orange","grapes")
-apple_prize=100
-orange_prize=140
-grapes_prize=80
-var kg=6
-console.log("the " +kg+ " Kg of apple is " +kg*apple_prize)
-console.log("the " +kg+ " Kg of orange is " +kg*orange_prize)
-console.log("the " +kg+ " Kg of grapes is " +kg*grapes_prize)
