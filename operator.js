109db00f88728322c8e00dd22ab76114278466de
ragavan59 <ragavan3518@gmail.com>
2025-09-19 09:56:46 +0530

    basic

---
+++ operator.js
@@ -0,0 +1,56 @@
+//product and thier prices
+var product=("apple","orange","grapes")
+apple_prize=100
+orange_prize=140
+grapes_prize=80
+var kg=6
+console.log("the " +kg+ " Kg of apple is " +kg*apple_prize)
+console.log("the " +kg+ " Kg of orange is " +kg*orange_prize)
+console.log("the " +kg+ " Kg of grapes is " +kg*grapes_prize)
+
+
+//addition operator
+var a=45
+var b=93
+var c=b+a 
+console.log("the additon of a and b is "+c)
+
+
+// subraction operator
+var d=84
+var e=34
+var f=d-e
+console.log("the subraction of d and e is "+f)
+
+
+//multiplication operator
+var g=21
+var h=13
+var j=g*h
+console.log("the multiply of g and h is "+j)
+
+
+//division operator
+var k=21
+var l=13
+var m=k/l
+console.log("the division of k and l is "+m)
+
+//increment before variable
+console.log("increment before ")
+var a=100
+var b=++a
+console.log(a)
+console.log(b)
+console.log(a)
+console.log(a)
+
+//increment after variable
+console.log(" increment of variable")
+var a=100
+var b=a++
+console.log(a)
+console.log(b)
+console.log(a)
+console.log(b)
+
---
+++ script.js
@@ -0,0 +1,9 @@
+
+var product=("apple","orange","grapes")
+apple_prize=100
+orange_prize=140
+grapes_prize=80
+var kg=6
+console.log("the " +kg+ " Kg of apple is " +kg*apple_prize)
+console.log("the " +kg+ " Kg of orange is " +kg*orange_prize)
+console.log("the " +kg+ " Kg of grapes is " +kg*grapes_prize)
